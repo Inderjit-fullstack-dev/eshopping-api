a9bf20ef5b2ea6ce9fd89c038a86e615f14d88f8
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-09-29 12:27:52 +0530

    PagedList Exception resolved

--- Catalog.Application/Helpers/PagedList.cs
+++ Catalog.Application/Helpers/PagedList.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace Catalog.Application.Helpers
 {
@@ -18,11 +20,15 @@ namespace Catalog.Application.Helpers
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
 
-        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber,
+        public static async Task<PagedList<T>> CreateAsync(IMongoQueryable<T> source, int pageNumber,
             int pageSize)
         {
             var count = await source.CountAsync();
-            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            var items = await source.Skip((pageNumber - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+            
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
