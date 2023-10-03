fc940e17075f78cc4e3cf3c9644954c03ad2992a
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-10-03 16:50:18 +0530

    added coupon proto

---
+++ Discount.gRPCS/Services/CouponService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace Discount.gRPCS.Services
+{
+    public class CouponService : CoupongRPCService.CoupongRPCServiceBase
+    {
+        public override Task<Coupon> GetCoupon(GetCouponRequest request, ServerCallContext context)
+        {
+            return base.GetCoupon(request, context);
+        }
+        public override Task<Coupon> AddCoupon(Coupon request, ServerCallContext context)
+        {
+            return base.AddCoupon(request, context);
+        }
+        public override Task<Coupon> UpdateCoupon(Coupon request, ServerCallContext context)
+        {
+            return base.UpdateCoupon(request, context);
+        }
+        public override Task<DeleteCouponResponse> DeleteCoupon(DeleteCouponRequest request, ServerCallContext context)
+        {
+            return base.DeleteCoupon(request, context);
+        }
+    }
+}
