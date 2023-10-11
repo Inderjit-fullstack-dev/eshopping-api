1e0af36cc1c8755e1eb97fdab175e10e3fc5e26d
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-10-11 17:35:33 +0530

    Discount Service Implemented

---
+++ Discount.Core/Entities/Coupon.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Discount.Core.Entities
+{
+    [Table("coupons")]
+    public class CouponEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string CouponCode { get; set; }
+        
+        [Required]
+        public string ProductId { get; set; }
+        
+        [Required]
+        public int Amount { get; set; }
+        public bool? IsApplied { get; set; }
+    }
+}
---
+++ Discount.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Application.Repositories;
+using Discount.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+namespace Discount.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+
+            // REPOSITORIES STARTS HERE
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+            // REPOSITORIES ENDS HERE
+
+            return services;
+        }
+    }
+}
